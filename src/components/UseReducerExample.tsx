f98cb8084841d8eab34d2cb933207e8ff9c2def1
JubaStriker <jubastriker@gmail.com>
2024-01-11 13:58:51 +0600

    Reducer executed

--- src/components/UseReducerExample.tsx
+++ src/components/UseReducerExample.tsx
@@ -1,14 +1,23 @@
 import { useReducer } from 'react'
+import Button from './Button'
+
+type TAction = {
+  type: string;
+  payload?: number;
+}
 
 const initialState = { count: 0 }
 
-const reducer = (currentState: { count: number }, action: { type: unknown }) => {
+const reducer = (currentState: typeof initialState, action: TAction) => {
   switch (action.type) {
     case "increment":
       return { count: currentState.count + 1 }
 
     case "decrement":
       return { count: currentState.count - 1 }
+
+    case "incrementBySetAmount":
+      return { count: currentState.count + action?.payload }
 
     default:
       return currentState;
@@ -17,15 +26,15 @@ const reducer = (currentState: { count: number }, action: { type: unknown }) => 
 }
 
 const UseReducerExample = () => {
-
   const [state, dispatch] = useReducer(reducer, initialState)
-  console.log(state)
-
   return (
     <div>
       <h1>{state.count}</h1>
       <button className='btn mr-3' onClick={() => dispatch({ type: 'increment' })}>Increment</button>
       <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+      <span className='ml-3' onClick={() => dispatch({ type: 'incrementBySetAmount', payload: 3 })}>
+        <Button title='Increment by 3' />
+      </span>
     </div>
   );
 };
