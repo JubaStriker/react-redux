857e30016ab54c88b1652a8707847bbfbeb9d64b
JubaStriker <jubastriker@gmail.com>
2024-01-21 16:39:51 +0600

    Type issue fixed

--- src/components/NormalForm/NormalForm.tsx
+++ src/components/NormalForm/NormalForm.tsx
@@ -1,9 +1,21 @@
-import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FieldValues, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const SignUpSchema = z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.string().email().min(1, "Email is required"),
+    password: z.string().min(8, "Too short")
+})
+
+type TNormalForm = z.infer<typeof SignUpSchema>
 
 const NormalForm = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm<TNormalForm>({
+        resolver: zodResolver(SignUpSchema)
+    });
 
-    const onSubmit = (data) => {
+    const onSubmit = (data: FieldValues) => {
         console.log(data)
     };
     return (
@@ -14,15 +26,17 @@ const NormalForm = () => {
                     className='w-full border border-gray-300 rounded-md focus:border-purple-500 focus:ring-purple-500'
                     type="text" id="name" {...register("name", { required: true })}
                 />
-                {errors?.name && <span className='text-xs text-red-500'>This field is required</span>}
+                {errors?.name && <span className='text-xs text-red-500'>{errors.name.message as string}</span>}
             </div>
             <div>
                 <label htmlFor="email">Email</label>
                 <input className='w-full border border-gray-300 rounded-md focus:border-purple-500 focus:ring-purple-500' type="text" id="email" {...register("email")} />
+                {errors?.email && <span className='text-xs text-red-500'>{errors.email.message as string}</span>}
             </div>
             <div>
                 <label htmlFor="password">Password</label>
                 <input className='w-full border border-gray-300 rounded-md focus:border-purple-500 focus:ring-purple-500' type="password" id="password" {...register("password")} />
+                {errors.password && <span className='text-xs text-red-500'>{errors.password.message as string}</span>}
             </div>
             <button type='submit' className='btn btn-primary mt-2'>Submit</button>
         </form>
