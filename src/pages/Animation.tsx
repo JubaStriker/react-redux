152655cb8e8ba14242838fe2cfadc5dccb06e40c
JubaStriker <jubastriker@gmail.com>
2024-01-22 01:12:29 +0600

    CSS added

--- src/pages/Animation.tsx
+++ src/pages/Animation.tsx
@@ -1,10 +1,14 @@
 import '../App.css'
 const Animation = () => {
     return (
-        <div className="main">
-            <div className="box-1"></div>
-            <h1>Hidden text</h1>
-        </div>
+        <>
+            <div className="main">
+                <div className="box-1"></div>
+                <h1>Hidden text</h1>
+                <div className='box-2'></div>
+            </div>
+
+        </>
     );
 };
 
