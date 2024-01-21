421ab4fe1f1e0e4ef566b8a2cc4e81d56c7966a6
JubaStriker <jubastriker@gmail.com>
2024-01-21 15:33:44 +0600

    New from fields added

--- src/components/NormalForm/NormalForm.tsx
+++ src/components/NormalForm/NormalForm.tsx
@@ -12,6 +12,14 @@ const NormalForm = () => {
                 <label htmlFor="name">Name</label>
                 <input type="text" id="name" {...register("name")} />
             </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" {...register("email")} />
+            </div>
+            <div>
+                <label htmlFor="password">Name</label>
+                <input type="text" id="password" {...register("password")} />
+            </div>
             <button type='submit'>Submit</button>
         </form>
     );
