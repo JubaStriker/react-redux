56418d4324d80147c5cb6021d5c792590008fcaa
JubaStriker <jubastriker@gmail.com>
2024-01-26 21:07:49 +0600

    Framer motion animation

--- src/pages/Animation.tsx
+++ src/pages/Animation.tsx
@@ -1,14 +1,40 @@
 import '../App.css'
+import { motion } from 'framer-motion';
+
+const parent = {
+    hidden: { opacity: 0, scale: 0.9 },
+    visible: { opacity: 1, scale: 1 }
+}
+
+const child = {
+    hidden: { opacity: 0, scale: 0.9 },
+    visible: { opacity: 1, scale: 1 }
+}
 const Animation = () => {
     return (
-        <>
-            <div className="main">
-                <div className="box-1"></div>
-                <h1>Hidden text</h1>
-                <div className='box-2'></div>
-            </div>
+        <div>
+            <motion.div
+                className='size-64 bg-indigo-500 rounded-lg flex flex-wrap justify-center items-center gap-3 p-5'
+                variants={parent}
+                initial="hidden"
+                animate="visible"
+                transition={{ ease: 'easeInOut', duration: 1.5, staggerChildren: 0.5, delayChildren: 0.5 }}
+            >
+                <motion.div
+                    variants={child}
+                    className='bg-sky-300 rounded-lg size-20' ></motion.div>
+                <motion.div
+                    variants={child}
+                    className='bg-sky-300 rounded-lg size-20' ></motion.div>
+                <motion.div
+                    variants={child}
+                    className='bg-sky-300 rounded-lg size-20' ></motion.div>
+                <motion.div
+                    variants={child}
+                    className='bg-sky-300 rounded-lg size-20' ></motion.div>
 
-        </>
+            </motion.div>
+        </div>
     );
 };
 
